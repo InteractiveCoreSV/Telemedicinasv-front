04f4a898a86d0cfd137d7262310a0a2a0fe04314
InteractiveCore-Projects <carranza@interactivecore.app>
2026-06-11 16:12:55 -0600

    Update environment.prod.ts
    se modifico environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  production: true,
-  portalUrl:'https://telemedicina.analizalabonline.com',
+  production: true,  
   urlApi: 'https://api-telemedicina.interactivecore.app/api',
   urlApiPure:'https://api-telemedicina.interactivecore.app/',
 
